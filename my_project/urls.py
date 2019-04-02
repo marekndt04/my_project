ebe8965e987f17a6cd610f1e5cf1ace4d18b3276
Marek Arendt <m.arendt04@gmail.com>
2019-04-02 18:57:18 +0200

    Main page html changed. Currentlu if user i logged show only logout button, and if not show login and register button.
    Register view changed from method to class.

--- my_project/urls.py
+++ my_project/urls.py
@@ -22,7 +22,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    path('register/', user_views.Register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login_page.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout_page.html'), name='logout'),
     path('', MainPage.as_view()),
--- users/views.py
+++ users/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views import View
 from .forms import UserRegisterForm
 
-def register(request):
 
-    if request.method == "POST":
+class Register(View):
+
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'users/registration_page.html', {'form': form})
+
+    def post(self, request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Konto stworzone dla {}!. Zaloguj się'.format(username))
             return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/registration_page.html', {'form': form})
-
+        else:
+            form = UserRegisterForm()
+        return render(request, 'users/registration_page.html', {'form': form})
