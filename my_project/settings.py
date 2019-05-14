707bbaf8775a1f6e61b73c130f3c0bdcacd57807
Marek Arendt <m.arendt04@gmail.com>
2019-05-14 19:12:08 +0200

    ssada

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^br0q)mt4=6@%%*wa(2dj%&46iejni!-t0o%u*v=6ke&c@*s7m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['marek.localhost.run', 'https://marek.localhost.run', '127.0.0.1']
 
