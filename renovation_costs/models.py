d09078fda181e9ceafb7f2a0a312d091bb59b044
Marek Arendt <m.arendt04@gmail.com>
2019-06-05 18:38:31 +0200

    - renovations_costs models.py : models for products used to make wallpapers created

--- renovation_costs/models.py
+++ renovation_costs/models.py
@@ -15,3 +15,15 @@ class Base(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     production_per_litr = models.DecimalField(max_digits=5, decimal_places=2)
     capacity = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Wallpaper(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    capacity = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class WallpaperGlue(models.Model):
+    name = models.CharField(max_length=128)
+    usage = models.DecimalField(max_digits=5, decimal_places=2)
+    capacity = models.DecimalField(max_digits=5, decimal_places=2)
