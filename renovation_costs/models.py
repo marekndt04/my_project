ba280d226069f4c66426555444868d0918b7903e
Marek Arendt <m.arendt04@gmail.com>
2019-06-05 11:09:49 +0200

    -renovations_costs new model for paints created -

--- renovation_costs/models.py
+++ renovation_costs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Paint(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    production_per_litr = models.DecimalField(max_digits=5, decimal_places=2)
+    capacity = models.DecimalField(max_digits=5, decimal_places=2)
